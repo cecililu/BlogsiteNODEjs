beeb9b800960d39873875b71e9f5519ba828d472
rupaak <rupakt525@gmail.com>
2022-08-26 21:53:24 +0545

    working form ejs

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const dbConnect = require("./service/db");
 const path = require("path");
 const app = express();
 
+const bodyParser = require("body-parser");
+
 const PORT = process.env.PORT || 8000;
 
 const staticPath = path.join(__dirname, "../public");
@@ -13,18 +15,27 @@ app.set("view engine", "ejs");
 app.use(express.static(staticPath));
 
 app.use(express.json());
+app.use(bodyParser.urlencoded())
 
 app.use("/api/v1", indexRouter);
 
 app.get("/", (req, res) => {
+  // res.render("index.ejs");
+
   res.send({
     data: "Server running smooth",
   });
 });
 
 app.get("/createblog", (req, res) => {
-  res.render("index");
+  res.render("index.ejs");
+
 });
+
+
+// app.post("/createblogtest", (req, res) => {
+//   console.log(req.body);
+// });
 
 app.all("*", (req, res) => {
   res.status(404).send({
